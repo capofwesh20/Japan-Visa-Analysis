a33b139d160017c79250e6ec38607b800eac3aa4
Ikenna Odezuligbo <35642413+capofwesh20@users.noreply.github.com>
2024-02-21 01:51:29 -0600

    Add files via upload

---
+++ download_files.sh
@@ -0,0 +1,1 @@
+scp -i spark-clusters_key.pem -rp azureuser@13.92.158.247:/home/azureuser/output source/
---
+++ upload_files.sh
@@ -0,0 +1,2 @@
+scp -i spark-clusters_key.pem -r ./source/* azureuser@13.92.158.247:/home/azureuser
+
